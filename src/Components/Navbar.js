5dba92f35672448ef406032a677e9228466f18e9
johnnybossboy <john_bosschert@hotmail.com>
2018-05-31 14:57:39 +0200

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
             <div className="container-fluid color-primary-bg navbar navbar--fixed">
                 <div className="row">
                     <div className="col-xs-12 text-center">
-                        <h1 className="text-uppercase text--title color-white">LabelA Proefopdracht</h1>
+                        <h1 className="text-uppercase text--title color-white">Proefopdracht</h1>
                     </div>
                 </div>
             </div>
